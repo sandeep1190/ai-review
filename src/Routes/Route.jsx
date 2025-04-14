e916bef762c0545ab2e486c2fff4c8f7a4e318d5
Sandeep Dhiman <sandeep.dhiman@exate.com>
2025-04-14 16:31:01 +0530

    add dashboard router

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -3,12 +3,14 @@ import Layout from "../components/Layout/Layout";
 import Review from "../pages/Review/Review";
 import Location from "../pages/Location/Location";
 import SignIn from "../pages/Authentication/SignIn";
+import Dashboard from "../pages/Dashboard/Dashboard"; 
 
 const Routing = () => {
     return (
         <Routes>
             <Route path="/" element={<SignIn />} />
             <Route element={<Layout />}>
+                <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/reviews" element={<Review />} />
                 <Route path="/locations" element={<Location />} />
             </Route>
